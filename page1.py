23bf6e2c3005998a7242e778bb4218fc294c9a28
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-01 01:40:45 +0900

    update page1

--- page1.py
+++ page1.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import llm_manager
 
 def app():
     st.title('Page 1')
     st.write('This is page 1')
+
+    text = st.text_input('Input Text: ')
+    if text:
+        embedding = llm_manager.get_embedding(text)
+        st.write(embedding)
 
