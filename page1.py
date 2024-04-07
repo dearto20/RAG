2ff1b06067fac4d2a4e7ee588cb27dcb3bf0d3f4
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-08 05:16:50 +0900

    Update page1.py

--- page1.py
+++ page1.py
@@ -3,10 +3,9 @@ import llmmanager
 
 def app():
     st.title('Page 1')
-    st.write('This is page 1')
+    st.write('문장을 Embedding으로 변환')
 
     text = st.text_input('Input Text: ')
     if text:
-        embedding = llmmanager.get_embedding(text)
-        st.write(embedding)
+        st.write(llmmanager.get_embedding(text))
 
