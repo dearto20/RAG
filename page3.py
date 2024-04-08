f2cccb45ec74103c4863c742600e01a50e682e8c
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-08 15:28:13 +0900

    update

--- page3.py
+++ page3.py
@@ -26,27 +26,28 @@ def app():
     source = st.selectbox('Source', ('KG', 'Runestone', 'Etc'))
     chunk_size = int(st.radio("Chunk Size", ["16", "32", "64"]))
 
-    if st.button("Save Embeddings", on_click = store_sentence):
-        def store_sentence():
-            collection = db.get_or_create_collection('context')
-            def add_chunk(chunk, sentence):
-                embedding = llmmanager.get_embedding(chunk)
-                collection.add(ids = [str(uuid.uuid4())], embeddings = [embedding], metadatas = [{'chunk': chunk, 'sentence': sentence, 'source': source}])
-            
-            for sentence in re.split(r'\.|\n\n', context):
-                sentence = re.sub(r'\s{2, }', ' ', sentence).strip()
-                if not sentence:
-                    continue
+    def clear_text_area():
+        st.session_state['context'] = ""
 
-                st.write(f'Sentence: {sentence}')
-                add_chunk(sentence, sentence)
+    if st.button("Save Embeddings", on_click = clear_text_area):
+        collection = db.get_or_create_collection('context')
+        def add_chunk(chunk, sentence):
+            embedding = llmmanager.get_embedding(chunk)
+            collection.add(ids = [str(uuid.uuid4())], embeddings = [embedding], metadatas = [{'chunk': chunk, 'sentence': sentence, 'source': source}])
+        
+        for sentence in re.split(r'\.|\n\n', context):
+            sentence = re.sub(r'\s{2, }', ' ', sentence).strip()
+            if not sentence:
+                continue
 
-                if chunk_size > 0:
-                    words = re.findall(r'\S+', sentence)
-                    index = 0
-                    while cur_words := words[index:index + chunk_size]:
-                        index += chunk_size // 2
-                        chunk = ' '.join(cur_words)
-                        st.write(f'Chunk: {chunk}')
-                        add_chunk(chunk, sentence)
-            st.session_state['context'] = ""
+            st.write(f'Sentence: {sentence}')
+            add_chunk(sentence, sentence)
+
+            if chunk_size > 0:
+                words = re.findall(r'\S+', sentence)
+                index = 0
+                while cur_words := words[index:index + chunk_size]:
+                    index += chunk_size // 2
+                    chunk = ' '.join(cur_words)
+                    st.write(f'Chunk: {chunk}')
+                    add_chunk(chunk, sentence)
