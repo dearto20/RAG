508ceea7ebe2a3e6438803e143ddee41a417c0ba
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-08 17:42:19 +0900

    update

--- page3.py
+++ page3.py
@@ -43,7 +43,7 @@ def app():
                 words = re.findall(r'\S+', sentence)
                 index = 0
                 while cur_words := words[index:index + chunk_size]:
-                    index += chunk_size // 8
+                    index += chunk_size - (chunk_size // 4)
                     chunk = ' '.join(cur_words)
                     st.write(f'Chunk: {chunk}')
                     add_chunk(chunk, sentence) 
