140ee3a317199c33db3c703ad34e66cfdf4517bb
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-08 15:50:18 +0900

    update

--- page3.py
+++ page3.py
@@ -19,21 +19,18 @@ def app():
     st.divider()
     st.write('Add New Embeddings into DB')
 
-    if 'context' not in st.session_state:
-        st.session_state['context'] = ""
-
-    context = st.text_area('Sentences', key = 'context', value = st.session_state['context'])
+    context = st.text_area('Sentences')
     source = st.selectbox('Source', ('KG', 'Runestone', 'Etc'))
     chunk_size = int(st.radio("Chunk Size", ["16", "32", "64"]))
 
-    def clear_text_area():
+    if st.button("Save Embeddings"):
         collection = db.get_or_create_collection('context')
         def add_chunk(chunk, sentence):
             embedding = llmmanager.get_embedding(chunk)
             collection.add(ids = [str(uuid.uuid4())], embeddings = [embedding], metadatas = [{'chunk': chunk, 'sentence': sentence, 'source': source}])
+
         for sentence in re.split(r'\.|\n\n', context):
             sentence = re.sub(r'\s{2, }', ' ', sentence).strip()
-            st.write(f'Sentence : {sentence}')
             if not sentence:
                 continue
 
@@ -47,7 +44,4 @@ def app():
                     index += chunk_size // 2
                     chunk = ' '.join(cur_words)
                     st.write(f'Chunk: {chunk}')
-                    add_chunk(chunk, sentence)
-        st.session_state['context'] = ""
-
-    st.button("Save Embeddings", on_click = clear_text_area)
+                    add_chunk(chunk, sentence) 
