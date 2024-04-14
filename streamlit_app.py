cf4e89b67399ad1bd87043257ca790baf97575c7
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-14 14:51:43 +0900

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ PAGES = {
 }
 
 def main():
-    st.sidebar.title('Navigation')
+    st.sidebar.title('Indexing and Query Test With or Without Llamaindex')
     selection = st.sidebar.radio("Pages", list(PAGES.keys()))
 
     page = PAGES[selection]
