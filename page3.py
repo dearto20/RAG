93dfd900f0760f8f66ede12e7cb259b84626acec
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-14 17:09:17 +0900

    update

--- page3.py
+++ page3.py
@@ -1,7 +1,7 @@
 import llmmanager
 from llama_index.core import Document
 from llama_index.core import Settings
-from llama_index.core import StorageContext
+from llama_index.core import StorageContext, load_index_from_storage
 from llama_index.core import VectorStoreIndex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.openai import OpenAIEmbedding
