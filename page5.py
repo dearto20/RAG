7be223ad169f1bca34fc64227851002f7eb5d9a0
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-04-14 13:27:50 +0900

    update

--- page5.py
+++ page5.py
@@ -30,37 +30,13 @@ def app():
 
     if st.button("Save Embeddings"):
         collection = db.get_or_create_collection('context')
-        doc = Document(text=context)
-        documents = [doc]
         vector_store = ChromaVectorStore(chroma_collection = collection)
         storage_context = StorageContext.from_defaults(vector_store = vector_store)
+        doc = Document(text='context')
+        documents = [doc]
         index = VectorStoreIndex.from_documents(
             documents, storage_context = storage_context,
             transformations = [SentenceSplitter(chunk_size=chunk_size)]
         )
-
-
-        
-        def add_chunk(chunk, sentence):
-            embedding = llmmanager.get_embedding(chunk)
-            collection.add(ids = [str(uuid.uuid4())], embeddings = [embedding], metadatas = [{'chunk': chunk, 'sentence': sentence, 'source': source}])
-
-        count = 0
-        for sentence in re.split(r'\.|\n\n', context):
-            sentence = re.sub(r'\s{2, }', ' ', sentence).strip()
-            if not sentence:
-                continue
-
-            count = count + 1
-            #st.write(f'Sentence {count}: {sentence}')
-            #add_chunk(sentence, sentence)
-
-            if chunk_size > 0:
-                words = re.findall(r'\S+', sentence)
-                index = 0
-                while cur_words := words[index:index + chunk_size]:
-                    index += chunk_size - (chunk_size // 4)
-                    chunk = ' '.join(cur_words)
-                    st.write(f'Chunk: {chunk}')
-                    add_chunk(chunk, sentence) 
+    
         st.toast('Save Finished')
